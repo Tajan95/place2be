5e9d7aa51bd10280dfef78a38988b218eda3a3ae
Tajan95 <147590023+Tajan95@users.noreply.github.com>
2026-07-12 23:08:47 +0200

    Add domain model for place categories

---
+++ app/src/main/java/de/place2be/domain/model/PlaceCategory.kt
@@ -0,0 +1,14 @@
+package de.place2be.domain.model
+
+/**
+ * Grobe Kategorie eines öffentlichen Ortes.
+ */
+enum class PlaceCategory {
+    PARK,
+    SQUARE,
+    PROMENADE,
+    SHOPPING_STREET,
+    SHOPPING_CENTER,
+    DISTRICT,
+    OTHER_PUBLIC_PLACE,
+}
